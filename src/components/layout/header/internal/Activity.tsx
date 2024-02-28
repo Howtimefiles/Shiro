bb25e1d9f3496e6e2ffbdee6f4f516e9e31939e9
howtimefiles <1126632753@qq.com>
2024-02-28 17:34:03 +0800

    回滚

--- src/components/layout/header/internal/Activity.tsx
+++ src/components/layout/header/internal/Activity.tsx
@@ -31,7 +31,7 @@ const ActivityIconContext = createContext<{
   appDescription: any
 }>(null!)
 
-const CND_DOMAIN = 'https://fastly.xiaozhengyang.com/app'
+const CND_DOMAIN = 'https://fastly.jsdelivr.net/gh/Howtimefiles/reporter-assets@main'
 const fetchJsonData = () => {
   return Promise.all([
     fetch(`${CND_DOMAIN}/app-icon.json`).then((res) => res.json() as object),
